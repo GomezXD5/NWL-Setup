4ad970a940c139523972ce53da6d796fbc18920b
Gabriel <gomesgabriel2017@gmail.com>
2023-02-07 23:51:11 -0300

    finalizando as configurações iniciais

--- Explorer/SERVER/scr/server.ts
+++ Explorer/SERVER/scr/server.ts
@@ -1,11 +1,22 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
+app.register(cors)
 
-app.get('/hello', () => {
-    return 'Hello NLW'
+app.get('/hello', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
 })
 
-app.listen({
-    port: 3333,})  
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!!')
+  })
